9c75249d9ba1c4b54207d545853b888fad24cde3
veeramuthuk <118426156+veeramuthuk@users.noreply.github.com>
2023-03-09 18:29:40 +0530

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,33 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+document.getElementById('length').addEventListener('change', charLength)
+
+
+function charLength({
+
+    let chars = "abc123567";
+    let length = 9;
+    let password = "";
+
+    for(let i=0 i<=charLength;i++){
+
+        let number = password * charLength
+        password += str (password)
+
+    }
+
+    document.getElementById("password").value = password;
+
+
+})
+
+
+
